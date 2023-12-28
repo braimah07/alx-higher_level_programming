4909b665006bef8e26545b34a4a99b6a8279176e
kelly braimah <braimahkellly@gmail.com.com>
2023-12-28 19:27:54 +0100

    My commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# py file by me
+"""Class Square
+
+   Empty Class"""
+
+
+class Square:
+    """Does nothing"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# py file by me
+"""Class Square
+
+   Empty Class"""
+
+
+class Square:
+    """Defines a Square"""
+    def __init__(self, size):
+        """Initializing Square Size"""
+        self.__size = size
